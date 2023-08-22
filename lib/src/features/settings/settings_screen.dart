edac85984fdd3ec1393c4cae1a56a7d6a1ba4e41
desh98 <73052432+desh98@users.noreply.github.com>
2023-08-22 23:22:04 +0530

    Updated settings screen

--- lib/src/features/settings/settings_screen.dart
+++ lib/src/features/settings/settings_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:moodup/constants/colors.dart';
-import '../constants/sizes.dart';
+import 'package:moodup/pages/settings/abouthelp.dart';
+import 'package:moodup/pages/settings/account_settings.dart';
+import 'package:moodup/pages/settings/language.dart';
+import 'package:moodup/pages/settings/moodtracking.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -15,18 +19,16 @@ class SettingsPage extends StatelessWidget {
           title: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: const [
-              Icon(Icons.arrow_back),
-              SizedBox(width: 8),
               Text('Settings'),
             ],
           ),
         ),
       ),
-      body: _buildSettingsList(),
+      body: _buildSettingsList(context),
     );
   }
 
-  Widget _buildSettingsList() {
+  Widget _buildSettingsList(BuildContext context) {
     return ListView(
       children: [
         Padding(
@@ -35,13 +37,20 @@ class SettingsPage extends StatelessWidget {
             leading: Icon(Icons.account_circle),
             title: Text('Account'),
             subtitle: Text('Edit Profile, Change Password'),
-            onTap: () {},
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        AccountPage()), // Navigate to AccountPage
+              );
+            },
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: ListTile(
-            leading: Icon(Icons.lightbulb_outline),
+            leading: Icon(Icons.lightbulb_circle_sharp), // Leading Icon
             title: Text('Dark Mode'),
             subtitle: Text('Switch between light and dark modes'),
             trailing: Switch(
@@ -57,10 +66,15 @@ class SettingsPage extends StatelessWidget {
           child: ListTile(
             leading: Icon(Icons.timer),
             title: Text('Mood Tracking'),
-            subtitle: Text(
-              'Mood reminder, Mood log time',
-            ),
-            onTap: () {},
+            subtitle: Text('Mood reminder, Mood log time'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        MoodTrackingPage()), // Navigate to AccountPage
+              );
+            },
           ),
         ),
         Padding(
@@ -68,10 +82,15 @@ class SettingsPage extends StatelessWidget {
           child: ListTile(
             leading: Icon(Icons.language),
             title: Text('Language'),
-            subtitle: Text(
-              'Choose your preferred app language.',
-            ),
-            onTap: () {},
+            subtitle: Text('Choose your preferred app language.'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        LanguagePage()), // Navigate to AccountPage
+              );
+            },
           ),
         ),
         Padding(
@@ -80,21 +99,22 @@ class SettingsPage extends StatelessWidget {
             leading: Icon(Icons.help),
             title: Text('About and Help'),
             subtitle: Text('App Version, Help Center'),
-            onTap: () {},
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        AboutHelpPage()), // Navigate to AccountPage
+              );
+            },
           ),
         ),
       ],
     );
   }
 
-  bool _getOption1Value() {
-    return false;
-  }
-
-  void _setOption1Value(bool newValue) {}
-
   bool _getOption2Value() {
-    return true;
+    return false;
   }
 
   void _setOption2Value(bool newValue) {}
